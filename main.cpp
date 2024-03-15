62f48e4deeeabb677acca83a3947813a36fae501
oosio57 <urio@live.cn>
2024-03-15 08:31:57 +0800

    update macos_branch

--- main.cpp
+++ main.cpp
@@ -7,39 +7,18 @@ int main() {
     Tools tools = Tools();
     // // 读取原始图片
     //cv::Mat originalImage = cv::imread("E:\\QtProjs\\PartitionOfImage\\1.jpg",cv::IMREAD_UNCHANGED);
-    cv::Mat inputImage = cv::imread("/Users/lpcw/QtProjs/automatic-image-segmentation/1.jpg",cv::IMREAD_COLOR);
-    cv::Mat  originalImage = tools.toAlphaImage(inputImage); //.clone();
+    cv::Mat originalImage = cv::imread("/Users/lpcw/QtProjs/automatic-image-segmentation/1.jpg",cv::IMREAD_COLOR);
+    //cv::Mat  originalImage = tools.toAlphaImage(inputImage); //.clone();
 
-    // // 定义划分份数N和M
-    // int N = 3; // 宽度划分份数
-    // int M = 2; // 高度划分份数
-
-    // // 计算小图片的宽度和高度
-    // int smallWidth = originalImage.cols / N;
-    // int smallHeight = originalImage.rows / M;
-
-    // // 划分原始图片为N*M个小图片
-    // for (int i = 0; i < N; i++) {s
-    //     for (int j = 0; j < M; j++) {
-    //         cv::Rect roi(i * smallWidth, j * smallHeight, smallWidth, smallHeight);
-    //         cv::Mat smallImage = originalImage(roi).clone();
-
-    //         // 保存小图片
-    //         std::string filename = "small_image_" + std::to_string(i) + "_" + std::to_string(j) + ".jpg";
-    //         cv::imwrite(filename, smallImage);
-    //     }
-    // }
-    //Tools tools = Tools();
-    //tools.toTestSideCode(3,5);
     std::vector<PieceOfImage> smallImages = tools.toPartition(originalImage,2,2);
-    //int i=0;
+    int i=0;
     for(PieceOfImage smallImage:smallImages){
 
-        smallImage.setKeyPoints();
-        cv::Mat image = smallImage.image.clone();
+        //smallImage.setKeyPoints();
+        cv::Mat image = smallImage.shapedImage.clone();
         KeyPoints keyPoints = smallImage.getKeyPoints();
-/*
-        cv::rectangle(image, keyPoints.externalScope.ltp, keyPoints.externalScope.rbp,cv::Scalar(255, 0, 0), 2);
+
+        cv::rectangle(image, keyPoints.externalScope.ltp, keyPoints.externalScope.rbp,cv::Scalar(0, 0, 255), 2);
         cv::rectangle(image, keyPoints.internalScope.ltp, keyPoints.internalScope.rbp, cv::Scalar(255, 0, 0), 2); // 左上角和右下角坐标，颜色，线宽
 
         // 在图片上绘制圆形
@@ -57,8 +36,8 @@ int main() {
         cv::imshow("Image with shapes", image);
         cv::waitKey(0);
         cv::destroyAllWindows();
-*/
-        smallImage.image=image.clone();
+
+        smallImage.shapedImage=image.clone();
 
         tools.toShapeImage(smallImage);
     }
--- tools.h
+++ tools.h
@@ -4,12 +4,20 @@
 
 class SizeOfPartition{
 public:
-    SizeOfPartition();
+    SizeOfPartition(){
+        col = 0;
+        row = 0;
+        width = 0;
+        height = 0;
+        lta = 0;
+        keyPoints = KeyPoints();
+    };
     int col;
     int row;
     int width;
     int height;
     int lta;
+    KeyPoints keyPoints;
 };
 
 class Tools
@@ -19,8 +27,9 @@ public:
     int randCode();
     std::vector<PieceOfImage> toSideCode(std::vector<PieceOfImage> images,const int cols);
     void toTestSideCode(const int rows,const int cols);
-    std::vector<PieceOfImage> toPartition(const cv::Mat &image,int rows,int cols);
+    std::vector<PieceOfImage> toPartition(const cv::Mat image,int rows,int cols);
     std::string setType(Location loc,int rows,int cols);
+    KeyPoints setCircleScopes(KeyPoints keyPoints,int lta,std::map<std::string,int> sidesCode);
     SizeOfPartition setSizeOfPartition(const std::string type,
                                        const std::map<std::string,int> sidesCode,
                                        const int col,const int row,
@@ -30,7 +39,7 @@ public:
     bool inScope(cv::Point point,CircleScope scope);
     int inScopes(cv::Point point,std::vector<CircleScope> circleScopes);
     void toShapeImage(PieceOfImage &image);
-    cv::Mat toAlphaImage(const cv::Mat &inputImage);
+    cv::Mat toAlphaImage(const cv::Mat inputImage);
 };
 
 #endif // TOOLS_H
